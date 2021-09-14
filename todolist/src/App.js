9e9fcd5a426f526dda8e95c9fb9ebf07a1d1ab69
Herocode <thaiduong61130@gmail.com>
2021-09-14 14:32:15 +0700

    Buoc-2

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import react from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: "", tasks: [] };
+  }
+  myChangeTaskHandler = (event) => {
+    //console.log(event.target.value);
+    this.setState({ task: event.target.value });
+  };
+  addTaskHandler = (event) => {
+    console.log(this.state.task);
+    this.state.tasks.push(this.state.task);
+    let newTask = this.state.tasks;
+    console.log(newTask);
+    this.setState({ tasks: newTask });
+    this.state.task = "";
+    console.log(this.state.task);
+  };
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <h1>TodoList</h1>
+        <header className="App-header">
+          <img
+            src="/assest/add.png"
+            alt="add task"
+            width="30px"
+            style={{ cursor: "pointer" }}
+            title="Bấm Để thêm Task"
+            onClick={() => this.addTaskHandler()}
+          />
+          <input
+            value={this.state.task}
+            type="text"
+            onChange={this.myChangeTaskHandler}
+          />
+        </header>
+        <ul style={{ listStyle: "none" }}>
+          {this.state.tasks.map((val, index) => {
+            return <li key={index}>{val}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
