591e2801bd4058805896c4f48ffca4e47d406273
Herocode <thaiduong61130@gmail.com>
2021-09-18 09:24:52 +0700

    ok

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,68 +1,34 @@
 import "./App.css";
 import React from "react";
 import react from "react";
-import Todo from "./Todo";
+import TodoList from "./TodoList";
 class App extends react.Component {
   constructor(props) {
     super(props);
     this.state = { task: "", tasks: [] };
   }
-  myChangeTaskHandler = (event) => {
-    //console.log(event.target.value);
-    this.setState({ task: event.target.value });
-  };
-  addTaskHandler = (event) => {
-    if(this.state.task === ' ')
-      return ;
-    const id = this.state.tasks.length
-    const name = this.state.task
-    this.state.tasks.push({id,value:name,done:false})
-    let newTasks = this.state.tasks
-    this.setState({ tasks: newTasks });
-    this.state.task = "";
-    console.log(newTasks)
-  };
 
-  deleteTask = (id) => {
-      let newTasks = this.state.tasks.filter(task => task.id !== id)
-      this.setState({tasks:newTasks})
-  }
-
-  completeTask = (id) => {
-    this.state.tasks.forEach(task => {
-      if(task.id === id){
-        task.done = true
-      }
-    })
-    let newTasks = this.state.tasks
-    this.setState({tasks:newTasks})
-    console.log(newTasks)
-  }
   render() {
     return (
       <div className="App">
-        <br />
-        <h1>TodoList</h1>
-        <header className="App-header">
-          <img
-            src="/assest/add.png"
-            alt="add task"
-            width="30px"
-            style={{ cursor: "pointer" }}
-            title="Bấm Để thêm Task"
-            onClick={() => this.addTaskHandler()}
-          />
-          <input
-            value={this.state.task}
-            type="text"
-            onChange={this.myChangeTaskHandler}
-          />
-        </header>
-        <ul style={{ listStyle: "none",padding:0 }}>
-          {this.state.tasks.map((val, index) => {
-            return <Todo key={index} id={val.id} value={val.value} deleteTask={this.deleteTask} completeTask={this.completeTask} />;
-          })}
-        </ul>
+        <table class="table" border="1">
+          <thead>
+            <tr>
+              <th scope="col">Nhiệm vụ 1</th>
+              <th scope="col">Nhiệm vụ 2</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">
+                  <TodoList name="Đi chợ" />
+              </th>
+              <th scope="row">
+                  <TodoList name="Đi Shopping" />
+              </th>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
--- todolist/src/Todo.js
+++ todolist/src/Todo.js
@@ -4,29 +4,44 @@ import React from "react";
 class Todo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { task: "", tasks: [] };
+    this.state = { id: props.id, value: props.value, done: props.done };
   }
+
+  completeTask = () => {
+    this.setState({ done: true });
+    this.props.completeTask(this.state.id);
+  };
   render() {
-    return (
-      <div className="Todo aligned">
+    let checkDone = "";
+    if (!this.state.done) {
+      checkDone = (
         <img
           src="/assest/complete.png"
           alt="add task"
           width="30px"
           style={{ cursor: "pointer" }}
           title="Hoàn Thành"
-          onClick = {(e) => this.props.completeTask(this.props.id)}
+          onClick={(e) => this.completeTask()}
         />
+      );
+    } else{
+      checkDone = ""
+    }
+    return (
+      <div className="Todo aligned">
+        {checkDone}
         <img
           src="/assest/delete.png"
           alt="add task"
           width="30px"
           style={{ cursor: "pointer" }}
           title="Xóa"
-          onClick = {(e) => this.props.deleteTask(this.props.id)}
+          onClick={(e) => this.props.deleteTask(this.props.id)}
         />
         &nbsp; &nbsp;
-        <span>{this.props.value}</span>
+        <span className={this.state.done ? "done" : ""}>
+          {this.props.value}
+        </span>
       </div>
     );
   }
